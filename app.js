824090d0bca7c3e3dc09cd333bfe29e6b0a0dfd0
zakk-io <mozakk.io@gmail.com>
2024-11-10 08:45:52 +0200

    mogodb connection

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const path = require('path')
+const mongoose = require('mongoose')
+require('dotenv').config();
 const app = express()
 
 
+
 app.use(express.static(path.join(__dirname,'public')))
+
+const mongoUri = process.env.MONGO_URI;
+mongoose.connect(mongoUri,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => console.log('Connected!'));
 
 
 
